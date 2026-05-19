231eb9479e1052820c14f8b0f690a3d51f1fd4d9
aadheshofficial <aadheshoffcial@gmail.com>
2026-05-19 14:26:45 +0530

    Auto-append /api to baseURL in axios config and update .env

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+let baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+if (baseURL && !baseURL.endsWith('/api') && !baseURL.endsWith('/api/')) {
+  baseURL = baseURL.replace(/\/$/, '') + '/api';
+}
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', 
+  baseURL: baseURL, 
 });
 
 // Request interceptor for adding the bearer token
